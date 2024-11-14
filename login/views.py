7ea7b2f8df95903d92381b078d6ce8ff8a4e8616
Viale <blviale0@gmail.com>
2024-11-14 01:28:03 -0300

    test

--- login/views.py
+++ login/views.py
@@ -1,13 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 # Create your views here.
-def saludo(resquest):
-    document = """<html>
-    <head>
-    Hola
-    </head>
-    <body>
-    </body>
-    </html>"""
-    return HttpResponse(document)
+def home(request):
+    return render(request, "login/base.html")
+
+#end snippet
