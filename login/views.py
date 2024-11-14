07c22448999e3cd5a774f22e5c13e2eade4a4479
Viale <blviale0@gmail.com>
2024-11-14 00:10:36 -0300

    base

--- login/views.py
+++ login/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def saludo(resquest):
+    document = """<html>
+    <head>
+    Hola
+    </head>
+    <body>
+    </body>
+    </html>"""
+    return HttpResponse(document)
